824acba3d501cfaf0e1be2768403f8d2279076cd
Cole <95713039+chughes741@users.noreply.github.com>
2023-08-04 20:47:05 -0400

    17 add information to log headers (#22)
    * Updated setup to add header to logfiles
    
    * Fixed lint error
    
    * Added automatic version bumping

--- openct/setup/setup.py
+++ openct/setup/setup.py
@@ -5,7 +5,9 @@ import os
 import time
 import logging
 import argparse
+import platform
 
+import openct
 from .config_schema import Config
 from .config import load_config
 
@@ -47,6 +49,14 @@ def parse_args():
 
 def init_logging(log_dir: str) -> None:
     log_file = os.path.join(log_dir, f"ocb_{time.strftime('%Y%m%d_%H%M%S')}.log")
+
+    header_info = f"Package Version: {openct.__version__}\n"
+    header_info += f"OS: {platform.system()} {platform.release()}\n"
+    header_info += f"Python Version: {platform.python_version()}\n"
+    header_info += "\n"
+
+    with open(log_file, "a", encoding="utf-8") as file:
+        file.write(header_info)
 
     logging.basicConfig(
         level=logging.INFO,
